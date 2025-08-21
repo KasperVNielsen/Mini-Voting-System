d4f2e2ff7ba3248fc53b03716cdf2d68fb27129a
Turbobent <kasp728h@edu.mercantec.dk>
2025-08-21 14:06:27 +0200

    print and user input

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    int a=0,b=0,noResult,totalVotes;
+    int a = 0,b = 0,noResult = 0,totalVotes;
+    printf("how many votes do you want?\n");
+    scanf("%d",&totalVotes);
+
+    
+
 
 }
