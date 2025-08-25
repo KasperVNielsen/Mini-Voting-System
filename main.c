6ae1015d84b55b78a9cfa3da0141577f88f451d5
Turbobent <kasp728h@edu.mercantec.dk>
2025-08-25 09:39:41 +0200

    can vote on 2 people now

--- main.c
+++ main.c
@@ -4,8 +4,28 @@ int main(){
     int a = 0,b = 0,noResult = 0,totalVotes;
     printf("how many votes do you want?\n");
     scanf("%d",&totalVotes);
+    char votersChoice;
 
-    
+    int i = 0;
 
+    while (i < totalVotes){
+        printf("who do you want to vote on a or b?\n");
+        scanf("c", votersChoice);
+        if (votersChoice == 'a'){
+            a++;
+        } else if (votersChoice == 'b'){
+            b++;
+        } else {
+            noResult++;
+        }
+
+        i++;
+    }
+    printf("\nVoting Results:\n");
+    printf("a: %d votes\n", a);
+    printf("b: %d votes\n", b);
+    printf("Invalid votes: %d\n", noResult);
+
+    return 0;
 
 }
